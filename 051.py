9aee818a4bb2574a53e7018928ccc5dd9d6ef544
Hiroshi Matsumoto <matsumoto@jnlp.org>
2013-01-04 11:20:57 +0900

    051.py

--- 051.py
+++ 051.py
@@ -1,2 +1,14 @@
 #! /usr/bin/env python
 #-*- encoding:utf-8 -*-
+#(51) 日本語の文章をCaboChaで係り受け解析し，ラティス形式（-f1オプション）の解析結果を得よ．
+#ref:http://nltk.googlecode.com/svn/trunk/doc/book-jp/ch12.html#cabocha
+import CaboCha
+cabocha = CaboCha.Parser('--charset=UTF8')
+sent = u"太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
+
+print "parseToString:"
+print cabocha.parseToString(sent)
+
+print "tree.toString:"
+tree = cabocha.parse(sent)
+print tree.toString(CaboCha.FORMAT_LATTICE)
